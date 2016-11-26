1991e534a74912f156f9d7f48ff02935b53b08d1
Danielius <danielius@wisemonks.com>
2016-11-26 18:02:13 +0200

    browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,14 @@ class ApplicationController < ActionController::Base
 
   def resource
     if params[:url].present?
+      url = URI.parse(params[:url])
+      session[:host] = "#{url.scheme}://#{url.host}"
+
       response = HTTParty.get(params[:url])
 
       render html: response.body.html_safe
     else
-      response = HTTParty.get("https://dezutes.lt/#{ [params[:path], params[:format]].join('.') }")
+      response = HTTParty.get("#{session[:host]}/#{ [params[:path], params[:format]].join('.') }")
 
       send_data response.body, type: response.headers.content_type, disposition: 'inline'
     end
