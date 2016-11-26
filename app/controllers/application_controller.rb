d914ecce1e34d7a9bceaf926a59ddb0c942d3e5a
simonasdev <simonasgildutis@gmail.com>
2016-11-26 17:10:29 +0200

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+
+  def resource
+    if params[:url].present?
+      response = HTTParty.get(params[:url])
+
+      render html: response.body.html_safe
+    else
+      response = HTTParty.get("https://dezutes.lt/#{ [params[:path], params[:format]].join('.') }")
+
+      send_data response.body, type: response.headers.content_type, disposition: 'inline'
+    end
+  end
 end
