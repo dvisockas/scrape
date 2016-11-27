2d340c71b37ca884a383c4c60770bed91c7176c0
simonasdev <simonasgildutis@gmail.com>
2016-11-27 15:03:12 +0200

    done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,4 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require selectorgadget_combined.min
 //= require_tree .
--- app/assets/javascripts/xpath.js
+++ app/assets/javascripts/xpath.js
@@ -17,22 +17,24 @@ function getElementsFromXPath (xpath, document) {
   return result;
 }
 
-
-function startXPathSelection(attribute) {
-  window.model = {};
-
-  window.onmouseup = function(event) {
-    // window.model[attribute]
-
-  }
-}
-
-$(function() {
-  $(document).on('click', '#addAttribute', function() {
-    window.currentAttribute = $('input#attributeName').val();
-    startXPathSelection(window.currentAttribute);
-  })
-});
-
 // input#attributeName
 //   span#addAttribute
+
+window.model = {};
+
+$(document).on('click', '.record', function () {
+  $(this).toggleClass('active');
+  var frameContent = $('#frame').contents(),
+      document = frameContent[0];
+
+  if ($(this).hasClass('active')) {
+    var s=document.createElement('div');s.innerHTML='Loading...';s.style.color='black';s.style.padding='20px';s.style.position='fixed';s.style.zIndex='9999';s.style.fontSize='3.0em';s.style.border='2px solid black';s.style.right='40px';s.style.top='40px';s.setAttribute('class','selector_gadget_loading');s.style.background='white';document.body.appendChild(s);s=document.createElement('script');s.setAttribute('type','text/javascript');s.setAttribute('src','https://dv0akt2986vzh.cloudfront.net/unstable/lib/selectorgadget.js');document.body.appendChild(s);
+    setTimeout(function () {
+      $('#frame').contents().find('#_sg_div').attr('style', 'bottom: 60px !important');
+    }, 4000);
+  } else {
+    var attrName = $(this).parent().find('input').val();
+    window.model[attrName] = frameContent.find('#_sg_div #_sg_path_field').val();
+    frameContent.find('#_sg_div input[value=X]').click();
+  }
+})
